d4c0054eb8dd1a3ef3f48d5821c5261d840dcdee
cloudedge <rob_hirschfeld@dell.com>
2012-10-04 22:23:25 -0700

    basic functions working (list, create, ref from barclamp)

--- crowbar_framework/app/controllers/fog_io_controller.rb
+++ crowbar_framework/app/controllers/fog_io_controller.rb
@@ -18,18 +18,41 @@ require 'fog'
 class FogIoController < BarclampController
   
   def connection
-    # this should go into a session eventually
-    connection = Fog::Compute.new(
-      :provider => 'OpenStack',
-      :openstack_username => 'crowbar',
-      :openstack_api_key => 'crowbar',
-      :openstack_auth_url => 'http://192.168.124.81:5000/v2.0/tokens',
-      :openstack_tenant => 'openstack'
-    )
+    # lookup information about keystone
+    configs = ProposalObject.find_proposals('keystone')
+    if configs.length > 0
+      config = configs.first["attributes"]["keystone"]
+      server = configs.first.elements["keystone-server"]
+      # this should go into a session eventually
+      connection = Fog::Compute.new(
+        :provider => 'OpenStack',
+        :openstack_username => config["admin"]["username"],
+        :openstack_api_key => config["admin"]["password"],
+        :openstack_auth_url => "http://#{server}:5000/v2.0/tokens",
+        :openstack_tenant => config["admin"]["tenant"]
+      )
+    end
   end
 
   def nodes
     @cloud = connection
+    if request.post? 
+      server = connection.servers.create(params)
+      flash[:notice] = I18n.t((server ? 'create_success' : 'create_fail'), :scope=>'fog_io.nodes')
+    end
+
+    @flavors = {}
+    @flavors_select = {}
+    @cloud.flavors.sort_by(&:name).each do |f|
+      @flavors_select[f.name] = f.id 
+      @flavors[f.id] = f.name
+    end
+    @images = {}
+    @images_select = {}
+    @cloud.images.sort_by(&:name).each do |i| 
+      @images_select[i.name] = i.id
+      @images[i.id] = i.name
+    end
     # create a connection
     respond_to do |format|
       format.html # index.html.haml
